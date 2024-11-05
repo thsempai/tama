acdc5eebf5671df7d50c01c83e1ce2cd660ba239
Thomas Stassin <thomas@teampanoptes.be>
2024-11-05 18:20:00 +0100

    test scale

--- source/game.lua
+++ source/game.lua
@@ -16,11 +16,11 @@ class("Game").extends()
 
 function Game:init()
     self.fg = SpriteWithAnimation("tamagochi")
+    pd.display.setScale(2)
     self.fg:add()
     self.fg:addState("pose", 1, 2, { tickStep = 6 }).asDefault()
     self.fg:changeState("pose", true)
-    self.fg:moveTo(200, 120)
-
+    self.fg:moveTo(pd.display.getWidth() / 2, pd.display.getHeight() / 2)
 
     self.fg:playAnimation()
 end
